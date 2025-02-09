f9102ae993c064a46df1c7b4796fa912427bafcc
Gabriel M. Chiapetti <163891454+gabrielmchiapetti@users.noreply.github.com>
2025-02-09 20:08:21 -0300

    Update bofetch.py to version 0.2

--- bofetch.py
+++ bofetch.py
@@ -1,3 +1,6 @@
+# bofetch 1.0 - read Github page :
+# https://github.com/gabrielmchiapetti/bofetch
+
 import os
 from os import system as sy
 import datetime
@@ -29,11 +32,18 @@ frq = psutil.cpu_freq()
 distro = str.capitalize(str(distro.name(pretty=True)))
 datetime = str(datetime.datetime.now())
 
+usr = psutil.users()[0]
+usrname = usr.name
+usrhost = platform.node()
+realusr = str(usrname) + "@" + str(usrhost)
+fullusr = f"\033[1;36m{str(usrname)}\033[m" + f"\033[1;35m@\033[m" + f"\033[1;32m{str(usrhost)}\033[m"
 
-print("    .--.    " + "  " + "\033[33;1m=\033[m"*64)
-print("   |o_o |   " + "  " + f"\033[37;1m• System:           \033[34;1m{distro}\033[m")
-print("   |:_/ |   " + "  " + f"\033[37;1m• Version:          \033[36;1m{platform.release()}\033[m")
-print("  //   \ \  " + "  " + f"\033[37;1m• Architecture:     \033[35m{inf.machine}\033[m")
-print(" (|     | ) " + "  " + f"\033[37;1m• RAM:              \033[32;1m{conv(mem.used)} / {conv(mem.total)} ({mem.percent}%)\033[m")
-print("/'\_   _/'\ " + "  " + f"\033[37;1m• CPU frequency:    \033[33;1m{frq.current:.2f}Mhz ({psutil.cpu_count(logical=False)} cores)\033[m")
-print("\___)=(___/ " + "  " + "\033[33;1m=\033[m"*18 + " " + datetime + " " + "\033[33;1m=\033[m"*18)
+calc_user = (50 - len(realusr)) // 2
+
+print("    .--.    " + "  " + "\033[1;33m=\033[m"*calc_user + " " + fullusr + " " + "\033[1;33m=\033[m"*calc_user)
+print("   |o_o |   " + "  " + f"\033[37;1m• System:           \033[1;34m{distro}\033[m")
+print("   |:_/ |   " + "  " + f"\033[37;1m• Kernel:           \033[1;36m{platform.release()}\033[m")
+print("  //   \ \  " + "  " + f"\033[37;1m• Architecture:     \033[1;35m{inf.machine}\033[m")
+print(" (|     | ) " + "  " + f"\033[37;1m• RAM:              \033[1;32m{conv(mem.used)} / {conv(mem.total)} ({mem.percent}%)\033[m")
+print("/'\_   _/'\ " + "  " + f"\033[37;1m• CPU frequency:    \033[1;33m{frq.current:.2f}Mhz ({psutil.cpu_count(logical=False)} cores)\033[m")
+print("\___)=(___/ " + "  " + "\033[1;33m=\033[m"*12 + " " + datetime + " " + "\033[1;33m=\033[m"*12)
